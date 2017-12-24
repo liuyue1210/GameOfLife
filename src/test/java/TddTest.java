795228f16ced8efd3475e706bd84f733555271e9
liuyue1210 <34572251+liuyue1210@users.noreply.github.com>
2017-12-24 19:34:33 +0800

    Update TddTest.java

--- src/test/java/TddTest.java
+++ src/test/java/TddTest.java
@@ -23,4 +23,30 @@ public class TddTest {
         }
         assertEquals(true,flag);
     }
+    
+    @Test
+    public void testGame2(){
+        Game game=new Game();
+        int[][] temp=new int[game.max][game.max];
+        int[][] gol=new int[game.max][game.max];
+        for (int i=0;i<game.max;i++){
+            for (int j=0;j<game.max;j++){
+                temp[i][j]=0;
+            }
+        }
+        temp[2][2]=1;
+        temp[2][3]=1;
+        temp[3][2]=1;
+        temp[3][3]=1;
+        gol=game.grow(temp);
+        boolean flag=true;
+        for (int i=0;i<game.max;i++){
+            for (int j=0;j<game.max;j++){
+                if (gol[i][j]!=temp[i][j]){
+                    flag=false;
+                }
+            }
+        }
+        assertEquals(true,flag);
+    }
 }
